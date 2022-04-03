45bee1371e950492ffcda0b35708c6a2e788be41
Alexander Hirdman <hirdman.alex@icloud.com>
2022-04-03 21:08:52 +0200

    Request logger

--- server/server.js
+++ server/server.js
@@ -16,11 +16,35 @@ const corsOptions = {
   optionSuccessStatus: 200,
 };
 
+const demoLogger = (req, res, next) => {
+  let current_datetime = new Date();
+  let formatted_date =
+    current_datetime.getFullYear() +
+    "-" +
+    (current_datetime.getMonth() + 1) +
+    "-" +
+    current_datetime.getDate() +
+    " " +
+    current_datetime.getHours() +
+    ":" +
+    current_datetime.getMinutes() +
+    ":" +
+    current_datetime.getSeconds();
+  let method = req.method;
+  let url = req.url;
+  let status = res.statusCode;
+  let log = `[${formatted_date}] ${method}:${url} ${status}`;
+  console.log(log);
+  next();
+};
+
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+app.use(demoLogger)
 
 app.get('/', (_, res) => {
   res.json('Hello Motherflowers!');
